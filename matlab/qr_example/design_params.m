b59454e3c66ee84b2d08cb864273aaa417e9661a
George Xiaojing Zhang <xiaozhan@ee.ethz.ch>
2011-11-14 14:26:10 -0800

    Uploaded .tex file for documentation

---
+++ matlab/qr_example/design_params.m
@@ -0,0 +1,281 @@
+%==========================================================================
+% Define all design parameters
+%==========================================================================
+function [...
+    N, Q, R, ...
+    dlqr_controlweight, maxadm_controlweight, ...
+    max_x, max_y, max_vx, max_vy, ...
+    max_z, min_z, max_vz, ...
+    max_pitch_cmd, max_roll_cmd, ...
+    max_thrust_cmd, min_thrust_cmd, ...
+    max_pitch, max_pitch_rate, max_roll, max_roll_rate, ...
+    state_uncert, ...
+    enable_learning, ALPHA, MU_factor, ...
+    uncertainty_block] = design_params(p,m)
+
+% Define parameters of MPC procedure
+N = 15; % MPC horizon
+
+weights_select = 0; % 0 = ball catching, 1 = testing, 2 = learning demo (back & forth)
+
+if weights_select == 0
+    Q = diag([...
+        20 ...  % x pos
+        1 ... % x vel
+        0.01 ... % pitch
+        0.01 ... % pitch rate
+        20 ...  % y pos
+        1 ... % y vel
+        0.01 ... % roll
+        0.01 ... % roll rate
+        20 ... % z pos
+        1  % z vel
+        ]); % Cost on states
+    Q = Q(1:p,1:p);
+    R = diag([...
+        1 ... % roll cmd
+        1 ... % pitch cmd
+        0.1 ... % thrust cmd
+        ]); % Cost on control
+    R = R(1:m,1:m);
+    
+    dlqr_controlweight = 8; % cost multiplier for 'baseline' feedback law ** maybe try 5
+    maxadm_controlweight = 100; % cost multiplier for generating LQR K that is
+    % used in terminal set computation
+    max_x = 2; % [m]
+    max_y = 3; % [m]
+    max_vx = 10; % [m/s]
+    max_vy = 10; % [m/s]
+    max_z = -0.13; % [m] NED convention
+    min_z = -3.0; % [m] NED convention
+    max_vz = 5; % [m/s]
+    max_pitch_cmd = deg2rad(35); % [rad]
+    max_roll_cmd = deg2rad(35); % [rad]
+    max_thrust_cmd = 18; % N
+    min_thrust_cmd = 0; %N
+    max_pitch = pi/2; %pi/4; % [rad]
+    max_pitch_rate = 4*pi; % [rad/s]
+    max_roll = pi/2; %pi/4; % [rad]
+    max_roll_rate = 4*pi; % [rad/s]
+
+
+elseif weights_select == 1
+    % This is what we tried on Aug 23, 2011:
+    
+    Q = diag([...
+        10 ...  % x pos
+        1 ... % x vel
+        0.01 ... % pitch
+        0.01 ... % pitch rate
+        10 ...  % y pos
+        1 ... % y vel
+        0.01 ... % roll
+        0.01 ... % roll rate
+        10 ... % z pos
+        0.01  % z vel
+        ]); % Cost on states
+    Q = Q(1:p,1:p);
+    R = diag([...
+        1 ... % roll cmd
+        1 ... % pitch cmd
+        0.1 ... % thrust cmd
+        ]); % Cost on control
+    R = R(1:m,1:m);
+    
+    dlqr_controlweight = 2; % cost multiplier for 'baseline' feedback law ** maybe try 5
+    maxadm_controlweight = 100; % cost multiplier for generating LQR K that is
+    % used in terminal set computation
+    max_x = 2; % [m]
+    max_y = 3; % [m]
+    max_vx = 2; % [m/s]
+    max_vy = 2; % [m/s]
+    max_z = 3; % [m]
+    max_vz = 5; % [m/s]
+    max_pitch_cmd = deg2rad(20); % [rad]
+    max_roll_cmd = deg2rad(20); % [rad]
+    max_thrust_cmd = 18; % N
+    min_thrust_cmd = 0; %N
+    max_pitch = pi/2; %pi/4; % [rad]
+    max_pitch_rate = 4*pi; % [rad/s]
+    max_roll = pi/2; %pi/4; % [rad]
+    max_roll_rate = 4*pi; % [rad/s]
+    
+elseif weights_select == 2
+    % Attempts to de-tune Aug 23, 2011:
+    Q = diag([...
+        60 ...  % x pos
+        1 ... % x vel
+        0.01 ... % pitch
+        0.01 ... % pitch rate
+        60 ...  % y pos
+        1 ... % y vel
+        0.01 ... % roll
+        0.01 ... % roll rate
+        60 ... % z pos
+        1  % z vel
+        ]); % Cost on states
+    Q = Q(1:p,1:p);
+    R = diag([...
+        1 ... % roll cmd
+        1 ... % pitch cmd
+        0.1 ... % thrust cmd
+        ]); % Cost on control
+    R = R(1:m,1:m);
+    
+    dlqr_controlweight = 8; % cost multiplier for 'baseline' feedback law ** maybe try 5
+    maxadm_controlweight = 100; % cost multiplier for generating LQR K that is
+    % used in terminal set computation
+    max_x = 2; % [m]
+    max_y = 3; % [m]
+    max_vx = 10; % [m/s]
+    max_vy = 10; % [m/s]
+    max_z = 3; % [m]
+    max_vz = 5; % [m/s]
+    max_pitch_cmd = deg2rad(35); % [rad]
+    max_roll_cmd = deg2rad(35); % [rad]
+    max_thrust_cmd = 18; % N
+    min_thrust_cmd = 0; %N
+    max_pitch = pi/2; %pi/4; % [rad]
+    max_pitch_rate = 4*pi; % [rad/s]
+    max_roll = pi/2; %pi/4; % [rad]
+    max_roll_rate = 4*pi; % [rad/s]
+
+end
+
+%%
+
+
+% Uncertainty bound on x+ (use same for x and y axes)
+axis_uncert = [...
+    0.01 ... % pos [m]
+    0.1 ... % vel [m/s]
+    0.01 ... % att [rad]
+    0.1 % att rate [rad/s]
+    ];
+vert_uncert = [...
+    0.01 ... % alt [m] % this should be conservative
+    0.1 ... % alt rate [m/s] % based on max_thrust*beta_max(7) = 18*0.0035 = 0.0630, and some conservatism
+    ];
+if p == 8
+    state_uncert = [axis_uncert axis_uncert]';
+elseif p == 10
+    state_uncert = [axis_uncert axis_uncert vert_uncert]';
+else
+    assert(0); % shouldn't happen
+end
+
+ALPHA = 0.99; % forgetting factor (closer to 1, 'forget' slower)
+enable_learning = 1;
+MU_factor = 0.001; % weight on prior model ** maybe try 0.01
+
+uncertain_struct_option = 13;
+
+% no longer used:
+% uncertainty_upper_bounds = 0.1*ones(p+m+1,1);
+% uncertainty_lower_bounds = -0.1*ones(p+m+1,1);
+
+% Uncertainty structure:
+uncertainty_block = zeros(p, p + m + 1); % has shape of [A B d]
+
+% By convention, uncertain_struct_option < 10 is for p = 8...
+if p == 10
+    assert(uncertain_struct_option >= 10);
+end
+
+if uncertain_struct_option == 0
+    % original
+    uncertainty_block = [...
+        %      A           B   d
+        0 0 0 0  0 0 0 0  0 0  1
+        0 0 0 0  0 0 0 0  0 0  1
+        0 0 0 0  0 0 0 0  0 0  1
+        0 0 1 1  0 0 0 0  1 0  1
+        
+        0 0 0 0  0 0 0 0  0 0  1
+        0 0 0 0  0 0 0 0  0 0  1
+        0 0 0 0  0 0 0 0  0 0  1
+        0 0 0 0  0 0 1 1  0 1  1
+        ];
+elseif uncertain_struct_option == 1
+    % new
+    uncertainty_block = [...
+        %      A           B   d
+        0 0 0 0  0 0 0 0  0 0  0
+        0 0 0 0  0 0 0 0  0 0  1
+        0 0 0 0  0 0 0 0  0 0  0
+        0 0 1 1  0 0 0 0  1 0  1
+        
+        0 0 0 0  0 0 0 0  0 0  0
+        0 0 0 0  0 0 0 0  0 0  1
+        0 0 0 0  0 0 0 0  0 0  0
+        0 0 0 0  0 0 1 1  0 1  1
+        ];
+elseif uncertain_struct_option == 2
+    % new
+    uncertainty_block = [...
+        %      A           B   d
+        0 0 0 0  0 0 0 0  0 0  0
+        0 0 1 0  0 0 0 0  0 0  1
+        0 0 1 1  0 0 0 0  1 0  0
+        0 0 1 1  0 0 0 0  1 0  1
+        
+        0 0 0 0  0 0 0 0  0 0  0
+        0 0 0 0  0 0 1 0  0 0  1
+        0 0 0 0  0 0 1 1  0 1  0
+        0 0 0 0  0 0 1 1  0 1  1
+        ];
+    % options for altitude dynamics included:
+elseif uncertain_struct_option == 11
+    % new
+    uncertainty_block = [...
+        %      A                B     d
+        0 0 0 0  0 0 0 0  0 0  0 0 0  0
+        0 0 0 0  0 0 0 0  0 0  0 0 0  1
+        0 0 0 0  0 0 0 0  0 0  0 0 0  0
+        0 0 1 1  0 0 0 0  0 0  1 0 0  1
+        
+        0 0 0 0  0 0 0 0  0 0  0 0 0  0
+        0 0 0 0  0 0 0 0  0 0  0 0 0  1
+        0 0 0 0  0 0 0 0  0 0  0 0 0  0
+        0 0 0 0  0 0 1 1  0 0  0 1 0  1
+        
+        0 0 0 0  0 0 0 0  0 0  0 0 1  1
+        0 0 0 0  0 0 0 0  0 0  0 0 1  1
+        ];
+elseif uncertain_struct_option == 12
+    % new
+    uncertainty_block = [...
+        %      A                B     d
+        0 0 0 0  0 0 0 0  0 0  0 0 0  0
+        0 0 0 0  0 0 0 0  0 0  0 0 0  1
+        0 0 0 0  0 0 0 0  0 0  0 0 0  0
+        0 0 1 1  0 0 0 0  0 0  1 0 0  1
+        
+        0 0 0 0  0 0 0 0  0 0  0 0 0  0
+        0 0 0 0  0 0 0 0  0 0  0 0 0  1
+        0 0 0 0  0 0 0 0  0 0  0 0 0  0
+        0 0 0 0  0 0 1 1  0 0  0 1 0  1
+        
+        0 0 0 0  0 0 0 0  0 0  0 0 0  0
+        0 0 0 0  0 0 0 0  0 0  0 0 1  1
+        ];
+    
+elseif uncertain_struct_option == 13
+    % new
+    uncertainty_block = [...
+        %      A                B     d
+        0 0 0 0  0 0 0 0  0 0  0 0 0  0
+        0 0 0 0  0 0 0 0  0 0  0 0 0  1
+        0 0 0 0  0 0 0 0  0 0  0 0 0  0
+        0 0 1 1  0 0 0 0  0 0  1 0 0  1
+        
+        0 0 0 0  0 0 0 0  0 0  0 0 0  0
+        0 0 0 0  0 0 0 0  0 0  0 0 0  1
+        0 0 0 0  0 0 0 0  0 0  0 0 0  0
+        0 0 0 0  0 0 1 1  0 0  0 1 0  1
+        
+        0 0 0 0  0 0 0 0  0 0  0 0 0  1
+        0 0 0 0  0 0 0 0  0 0  0 0 1  1
+        ];
+end
+
---
+++ src/qr_example.cpp
@@ -0,0 +1,113 @@
+// mainLBmpcTP.cpp
+// example file to test simple examples 
+// date: November 08, 2011
+// author: Xiaojing ZHANG
+
+// matrices are imported from binary file created by MATLAB
+
+
+#include <iostream>		// I-O
+#include <string>
+#include <Eigen/Dense>	// matrix computation
+#include "LBmpcTP.h"	// class template
+#include <time.h>
+
+using namespace Eigen;
+using namespace std;
+
+int main(int argc, const char* argv[])
+{
+  // ------ SPECIFY parameters -------
+  const int _N = 15; // MPC horizon
+  const int _m = 3; // #input
+  const int _n = 10; // #states
+  const int _nSt = 20; // # state constraints
+  const int _nInp = 6; // # state constraints
+  const int _nF_xTheta = 72; // # Omega constraints
+  const int _pos_omega = 1; // <= _N
+  const string default_fileName = "ConstrParam.bin";
+  string filename;
+  bool verbose = 0; // '0' if it should shut up
+  int status; // stores status code
+
+  if (argc == 1)
+  {
+    filename = default_fileName;
+  }
+  else if (argc == 2)
+  {
+    filename = argv[1];
+  }
+  else
+  {
+    cerr << "Usage: " << argv[0] << " [path-to-ConstrParam.bin]" << endl;
+    return 1;
+  }
+  // -------- object instantiation -----------
+  // fileName contains name of file with parameters
+  // bool verbose: 1 or 0
+  LBmpcTP<double, _n, _m, _N, _nSt, _nInp, _nF_xTheta, _pos_omega> myObj(filename.c_str(), verbose);
+
+  Matrix<double, _n, _n> A;
+  Matrix<double, _n, _m> B;
+  Matrix<double, _n, 1> s;
+  myObj.get_A(A);
+  myObj.get_B(B);
+  myObj.get_s(s);
+
+  // ----------- SPECIFY arguments for step() ---------------
+  // those matrices are computed externely
+  // -----------  sizes of matrices ---------------
+  Matrix<double, _n, _n> Lm; // n x n
+  Matrix<double, _n, _m> Mm; // n x m
+  Matrix<double, _n, 1> tm; // n x 1
+  Matrix<double, _n, 1> x_hat; // n x 1, state estimate
+  Matrix<double, _n, 1> x_star[_N]; // n x 1, [_N], tracking
+  Matrix<double, _m, 1> u_opt; // m x 1, optimal input is saved there
+
+  // -------- they are updated at each time step ------------
+  Lm.setZero();
+  //Lm << 1, 2, 0, 1, 2, -2, 1.3, 1, 2, 2.3, 1, 2, -1, 0, -1, 1, 2, 2, -2.3, 1, 0, 0, 2, 1.4, -2;
+  Mm.setZero();
+//  Mm << 1, 1.4, 2, -1, 1, 2, 0, 0, 2, -1;
+
+  tm.setZero();
+//  tm << -1, 2, 1, 1, 2;
+//  x_hat << 3, 3, -2, 3, 4;
+  x_hat << 0, 0, 0, 0, 0, 0, 0, 0, -1.0, 0;
+
+  for (int i = 0; i <= _N - 1; i++)
+  {
+    x_star[i].setZero();
+    x_star[i][8] = -2.0;
+  }
+  // x_star_arg[0] << 29.1867 ,  20.3181,   36.6838,   10.5584,  -24.6923;
+  // x_star_arg[1] << 401.2845,  262.2318,  -73.8211, -285.3312,  391.4877;
+  // x_star_arg[2] << -1.4669*1000  ,  0.0849*1000 ,   0.1898*1000 ,   2.8506*1000 ,  -1.8977*1000;
+  // x_star_arg[3] << -0.8542*1000  ,  9.2976*1000   , 7.0920*1000 ,  -1.5346*1000 ,   4.6926*1000;
+
+
+  for (int j = 0; j < 1500; j++)
+  {
+    timespec start_rt;
+    timespec end_rt;
+    clock_gettime(CLOCK_REALTIME, &start_rt);
+    status = myObj.step(Lm, Mm, tm, x_hat, x_star);
+    clock_gettime(CLOCK_REALTIME, &end_rt);
+    double duration = (end_rt.tv_sec - start_rt.tv_sec) + 1e-9*(end_rt.tv_nsec - start_rt.tv_nsec);
+    cout << "status: " << status << " dur: " << duration;
+    if (!status)
+    {
+      u_opt = myObj.u_opt;
+      cout << " u_opt: " << u_opt.transpose();
+      cout << " x_hat: " << x_hat.transpose();
+      cout << endl;
+    }
+
+    x_hat = A * x_hat + B * u_opt + s;
+  }
+
+
+  return 0;
+}
+
