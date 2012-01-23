920a305e3ba8ed8f71288c3a99342c18d9a37dd6
George Xiaojing Zhang <xiaozhan@ee.ethz.ch>
2012-01-24 00:43:53 +0100

    slight modification in step size

---
+++ matlab/qr_example/pdiff.m
@@ -0,0 +1,18 @@
+% Algorithm from:
+% Theory and computation of disturbance invariant sets for discrete-time linear systems
+% Ilya Kolmanovsky and Elmer G. Gilbert
+
+function [F_d h_d] = pdiff(F_u, h_u, F_v, h_v)
+
+options = optimset('Display', 'off');
+
+N = length(h_u);
+h = zeros(N,1);
+
+for ind = 1:N
+	[foo, fval] = linprog(-F_u(ind,:),F_v,h_v,[], [], [], [], [], options);
+	h(ind) = -fval;
+end
+
+F_d = F_u;
+h_d = h_u - h;
