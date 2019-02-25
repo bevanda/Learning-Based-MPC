146e47f153e9b26ac86df430788ddb03db59dda7
Petar Bevanda <pbevanda@outlook.com>
2019-02-25 20:23:33 +0100

    Added ACADO implementation of the compressor surge control to work on

--- matlab/Moore-Greitzer_example/mgcm_acado.m
+++ matlab/Moore-Greitzer_example/mgcm_acado.m
@@ -1,31 +1,75 @@
-%% Start
-BEGIN_ACADO;
-acadoSet('problem_name','compressor_control_2D');
-%% Initial setting of the model
-DifferentialState x1 x2; Control u;
+clear;
+BEGIN_ACADO; %start ACADO
+    acadoSet('problemname', 'mgcm');
+    acadoSet('results_to_file',false);       % don't write results to file
+    %% Initial setting of the model
+    DifferentialState x1 x2; 
+    Control u;
+    
+    B = 1;
+    gamma = 1;
+    x10 = 0;
 
-f = acado.DifferenntialEquation();
-f.add(dot(x1) == 1/B*(x2-gamma*((x1+x10)^(1/2)-(x10)^(1/2))));
-f.add(dot(x2) == B*(-k3*x2^3-k2*x2^2-k1*x2-x1-u));
+    f = acado.DifferentialEquation();
+    
+    f.add(dot(x1) == 1/B*(x2-gamma*((x1+x10)^(1/2)-(x10)^(1/2))));
+    f.add(dot(x2) == B*(-1*x2^3-1*x2^2-1*x2-x1-u));
 
-%% Initial setting of the optimisation problem
-S = 1e2*eye(3); 
-S(1,1) = 1e2; 
-h = {u x1 x2} ; r = [0 0 0] ;
+    %% Initial setting of the optimisation problem
+    N = 15; %prediction horizon
+    sampling_time = 0.2;
+    
+    Q = 1e2*eye(3); 
+    Q(1,1) = 1e2; 
+    h = {u x1 x2}; 
+    r = [0,0,0];
+    
+    ocp = acado.OCP(0.0, N*sampling_time, 20) ;
+    ocp.minimizeLSQ(Q , h , r) ;
+    ocp.subjectTo(f);
+    ocp.subjectTo(0 <= x1 <= 1); 
+    ocp.subjectTo(1.1875 <= x2 <= 2.1547);
+    ocp.subjectTo(0.1547 <= u <= 2.1547);
+    
 
-ocp = acado.OCP(0.0, 20, 50) ;
-ocp.minimizeLSQ(S , h , r) ;
-ocp.subjectTo(f);
-ocp. subjectTo( 'AT START' , x1 == 0.1); 
-ocp.subjectTo('AT START ', x2 == 0);
-ocp.subjectTo(0 <= u <= 0.2);
+       %% Setting up the process
+    identity = acado.OutputFcn();
+    
+    dynamicSystem = acado.DynamicSystem(f, identity);
+    
+    process = acado.Process(dynamicSystem, 'INT_RK45');
+    
+    %% Setting up MPC controller
+    Duration = 10;
+    
+    algo = acado.RealTimeAlgorithm(ocp, sampling_time);
+    
+    algo.set( 'DISCRETIZATION_TYPE',         'MULTIPLE_SHOOTING' );
+    algo.set( 'MAX_NUM_ITERATIONS',          5                 );
+    algo.set( 'INFEASIBLE_QP_HANDLING',      'YES'             );
+    
+%     algo.set('KKT_TOLERANCE', 1e-5);
+%     algo.set('INTEGRATOR_TYPE', 'INT_RK45') 
+%     algo.set('HESSIAN_APPROXIMATION', 'GAUSS_NEWTON');
+%     algo.set('DISCRETIZATION_TYPE', 'COLLOCATION');
 
-%% Initial setup of the acado algorithm
-algo=acado.OptimizationAlgorithm(ocp);
-algo.set('KKT TOLERANCE', 1e-6);
-algo.set('INTEGRATOR TYPE', 'INT RK45');
-algo.set('HESSIAN APPROXIMATION', 'GAUSS NEWTON');
-algo.set('DISCRETIZATION TYPE', 'COLLOCATION');
-END_ACADO;
+    reference = acado.StaticReferenceTrajectory();
+    
+    
+    controller = acado.Controller(algo, reference);
+    
+    
+    sim = acado.SimulationEnvironment(0.0, Duration, process, controller );
+    
+    
+    r = [0.5,0.3,0.0];
+    
+    sim.init(r); 
+    
+    END_ACADO;       % Always end with "END_ACADO".
+                     % This will generate a file problemname_ACADO.m. 
+                     % Run this file to get your results. You can
+                     % run the file problemname_ACADO.m as many
+                     % times as you want without having to compile again.
 %% Run
-out = compressor_contorl_2D_RUN();
+out = mgcm_RUN();
