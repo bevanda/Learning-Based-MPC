09d2b3aa53fbff6ff4910c258f6b5d99419c8182
pbevanda <pbevanda@outlook.com>
2019-04-25 10:40:09 +0200

    LBMPC has a DARE solutino from anon stable AK

--- matlab/LBMPC/matOCP.m
+++ matlab/LBMPC/matOCP.m
@@ -7,7 +7,7 @@ function [Ks,Klqr,Q,R,P,T,Mtheta,LAMBDA,PSI,LAMBDA_0,PSI_0]=matOCP(A,B,C,n,m,o)
 p=[0.75, 0.78, 0.98, 0.99]; 
 [K,~,~] = place(A,B,p); %nominal feedback matrix
 Ks=-K;
-
+Klqr = -dlqr(A, B, Q, R);
 % Generate steady-state parameterization
 M = [A - eye(n), B, zeros(n,o); ...
         C, zeros(o,m), -eye(o)];
@@ -25,8 +25,6 @@ PSI_0 = M_0(n+1:n+m,:);
 % Define a nominal feedback policy K and corresponding terminal cost
 % 'baseline' stabilizing feedback law
 Q = eye(n); R = eye(m); % state and control penalties
-Klqr = -dlqr(A, B, Q, R);
-
 P = dare(A+B*Ks, B, Q, R); % terminal cost chosen as solution to DARE
 T = 1000; % terminal steady state cost
 
