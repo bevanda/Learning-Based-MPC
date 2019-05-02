9e5c952b90beba1ff5eb4631bd7faeb02e8459bf
pbevanda <pbevanda@outlook.com>
2019-05-02 18:55:44 +0200

    Plot updates

--- matlab/LBMPC/saved_data+plots/NMPCvsLBMPC.m
+++ matlab/LBMPC/saved_data+plots/NMPCvsLBMPC.m
@@ -9,62 +9,64 @@ NMPC50=load('NMPC_N50_sys_full.mat');
 
 %% Plot
 iterations=size(NMPC50.sysH,2)-1;
+Ts=0.01;
+t=Ts*(0:iterations);
 
 figure;
 subplot(5,1,1);
-plot(0:iterations, LBMPC40.sysH(1,:),'Linewidth',1.7,'Color','r'); hold on;
-plot(0:iterations, LBMPC50.sysH(1,:),'Linewidth',1.7,'Color','b'); hold on;
-plot(0:iterations, NMPC40.sysH(1,:),'Linewidth',1.7,'Color','r','LineStyle',':'); hold on;
-plot(0:iterations, NMPC50.sysH(1,:),'Linewidth',1.7,'Color','b','LineStyle',':'); hold on;
-plot(0:iterations,0*(0:iterations),'Linewidth',1.5,'LineStyle',':','Color','k'); 
+plot(t, LBMPC40.sysH(1,:),'Linewidth',1.7,'Color','r'); hold on;
+plot(t, LBMPC50.sysH(1,:),'Linewidth',1.7,'Color','b'); hold on;
+plot(t, NMPC40.sysH(1,:),'Linewidth',1.7,'Color','r','LineStyle',':'); hold on;
+plot(t, NMPC50.sysH(1,:),'Linewidth',1.7,'Color','b','LineStyle',':'); hold on;
+plot(t,0*(0:iterations),'Linewidth',1.5,'LineStyle',':','Color','k'); 
 grid on;
-xlabel('iterations');
+xlabel('time');
 ylabel('\delta x_1');
 title('mass flow');
 
 
 subplot(5,1,2);
-plot(0:iterations, LBMPC40.sysH(1,:),'Linewidth',1.7,'Color','r'); hold on;
-plot(0:iterations, LBMPC50.sysH(1,:),'Linewidth',1.7,'Color','b'); hold on;
-plot(0:iterations, NMPC40.sysH(1,:),'Linewidth',1.7,'Color','r','LineStyle',':'); hold on;
-plot(0:iterations, NMPC50.sysH(1,:),'Linewidth',1.7,'Color','b','LineStyle',':'); hold on;
-plot(0:iterations,0*(0:iterations),'Linewidth',1.5,'LineStyle',':','Color','k'); 
+plot(t, LBMPC40.sysH(1,:),'Linewidth',1.7,'Color','r'); hold on;
+plot(t, LBMPC50.sysH(1,:),'Linewidth',1.7,'Color','b'); hold on;
+plot(t, NMPC40.sysH(1,:),'Linewidth',1.7,'Color','r','LineStyle',':'); hold on;
+plot(t, NMPC50.sysH(1,:),'Linewidth',1.7,'Color','b','LineStyle',':'); hold on;
+plot(t,0*(0:iterations),'Linewidth',1.5,'LineStyle',':','Color','k'); 
 grid on;
-xlabel('iterations');
+xlabel('time');
 ylabel('\delta x_2');
 title('pressure rise');
 legend({'LBMPC N=40', 'LBMPC N=50', 'NMPC N=40','NMPC N=50'},'Location','northwest')
 
 subplot(5,1,3);
-plot(0:iterations, LBMPC40.sysH(3,:),'Linewidth',1.7,'Color','r'); hold on;
-plot(0:iterations, LBMPC50.sysH(3,:),'Linewidth',1.7,'Color','b'); hold on;
-plot(0:iterations, NMPC40.sysH(3,:),'Linewidth',1.7,'Color','r','LineStyle',':'); hold on;
-plot(0:iterations, NMPC50.sysH(3,:),'Linewidth',1.7,'Color','b','LineStyle',':'); hold on;
-plot(0:iterations,0*(0:iterations),'Linewidth',1.5,'LineStyle',':','Color','k'); 
+plot(t, LBMPC40.sysH(3,:),'Linewidth',1.7,'Color','r'); hold on;
+plot(t, LBMPC50.sysH(3,:),'Linewidth',1.7,'Color','b'); hold on;
+plot(t, NMPC40.sysH(3,:),'Linewidth',1.7,'Color','r','LineStyle',':'); hold on;
+plot(t, NMPC50.sysH(3,:),'Linewidth',1.7,'Color','b','LineStyle',':'); hold on;
+plot(t,0*(0:iterations),'Linewidth',1.5,'LineStyle',':','Color','k'); 
 grid on;
-xlabel('iterations');
+xlabel('time');
 ylabel('\delta x_3');
 title('throttle');
 
 subplot(5,1,4);
-plot(0:iterations, LBMPC40.sysH(4,:),'Linewidth',1.7,'Color','r'); hold on;
-plot(0:iterations, LBMPC50.sysH(4,:),'Linewidth',1.7,'Color','b'); hold on;
-plot(0:iterations, NMPC40.sysH(4,:),'Linewidth',1.7,'Color','r','LineStyle',':'); hold on;
-plot(0:iterations, NMPC50.sysH(4,:),'Linewidth',1.7,'Color','b','LineStyle',':'); hold on;
-plot(0:iterations,0*(0:iterations),'Linewidth',1.5,'LineStyle',':','Color','k'); 
+plot(t, LBMPC40.sysH(4,:),'Linewidth',1.7,'Color','r'); hold on;
+plot(t, LBMPC50.sysH(4,:),'Linewidth',1.7,'Color','b'); hold on;
+plot(t, NMPC40.sysH(4,:),'Linewidth',1.7,'Color','r','LineStyle',':'); hold on;
+plot(t, NMPC50.sysH(4,:),'Linewidth',1.7,'Color','b','LineStyle',':'); hold on;
+plot(t,0*(0:iterations),'Linewidth',1.5,'LineStyle',':','Color','k'); 
 grid on;
-xlabel('iterations');
+xlabel('time');
 ylabel('\delta x_4');
 title('throttle rate');
 
 subplot(5,1,5);
-plot(0:iterations, LBMPC40.sysH(5,:),'Linewidth',1.7,'Color','r'); hold on;
-plot(0:iterations, LBMPC50.sysH(5,:),'Linewidth',1.7,'Color','b'); hold on;
-plot(0:iterations, NMPC40.sysH(5,:),'Linewidth',1.7,'Color','r','LineStyle',':'); hold on;
-plot(0:iterations, NMPC50.sysH(5 ,:),'Linewidth',1.7,'Color','b','LineStyle',':'); hold on;
-plot(0:iterations,0*(0:iterations),'Linewidth',1.5,'LineStyle',':','Color','k'); 
+plot(t, LBMPC40.sysH(5,:),'Linewidth',1.7,'Color','r'); hold on;
+plot(t, LBMPC50.sysH(5,:),'Linewidth',1.7,'Color','b'); hold on;
+plot(t, NMPC40.sysH(5,:),'Linewidth',1.7,'Color','r','LineStyle',':'); hold on;
+plot(t, NMPC50.sysH(5 ,:),'Linewidth',1.7,'Color','b','LineStyle',':'); hold on;
+plot(t,0*(0:iterations),'Linewidth',1.5,'LineStyle',':','Color','k'); 
 grid on;
-xlabel('iterations');
+xlabel('time');
 ylabel('\delta u');
 title('Sys input');
 
--- matlab/LBMPC/saved_data+plots/utilities/response_info.m
+++ matlab/LBMPC/saved_data+plots/utilities/response_info.m
@@ -1,7 +1,7 @@
 function info=response_info(y,u,t,ys)
     T=t;
     plot(t,y); hold on;
-    info=stepinfo(y,T,ys);
+    info=stepinfo(y,T,ys,'SettlingTimeThreshold',0.05);
     info.CumError=compute_ctrl_error(y,T,ys,info.SettlingTime);
     info.CumEnergy=compute_ctrl_energy(u,t,info.SettlingTime);
     info.Overshoot=compute_overshoot(y,ys);
